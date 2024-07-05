2d24905cc488f1a4d1c2d895d31e67e269b0a20d
HAMOUDA\Hamouda <mohamed.hamouda7897@gmail.com>
2024-07-05 17:42:18 +0300

    app init

---
+++ android/app/src/main/kotlin/com/example/islami_c11_friday/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.islami_c11_friday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = 'Home';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Islami"),
+      ),
+    );
+  }
+}
